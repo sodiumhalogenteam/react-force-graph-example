bfc8a589da5f24f9862f0de2dbeaa1dc35e0f0e7
Zach Boatwright <szboatwright@gmail.com>
2019-02-12 15:25:55 -0600

    📦  NEW: add in basic node events, move graph settings into ForceGraph.js

--- src/ForceGraph.js
+++ src/ForceGraph.js
@@ -1,66 +1,133 @@
 import React, { Component } from "react";
 import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from "react-force-graph";
-import forceGraphSettings from "./forceGraphSettings";
 
 class ForceGraph extends Component {
-  state = {
-    graphType: 0, //options: 2d, 3d, or vr
-    amountOfNodes: 6,
-    graphData: {
-      nodes: [
-        {
-          id: "1",
-          name: "node 1",
-          val: 1
-        },
-        {
-          id: "2",
-          name: "node 2",
-          val: 3,
-          selected: true
-        },
-        {
-          id: "3",
-          name: "node 3",
-          val: 2
-        },
-        {
-          id: "4",
-          name: "node 4",
-          val: 5
-        },
-        {
-          id: "5",
-          name: "node 5",
-          val: 1
-        },
-        {
-          id: "6",
-          name: "node 6",
-          val: 2,
-          selected: true
+  constructor(props) {
+    super(props);
+    this.state = {
+      graphType: 0, //options: 2d, 3d, or vr
+      amountOfNodes: 6,
+      hoveredNode: null,
+      graphData: {
+        nodes: [
+          {
+            id: "1",
+            name: "node 1",
+            val: 1
+          },
+          {
+            id: "2",
+            name: "node 2",
+            val: 3,
+            selected: true
+          },
+          {
+            id: "3",
+            name: "node 3",
+            val: 2
+          },
+          {
+            id: "4",
+            name: "node 4",
+            val: 5
+          },
+          {
+            id: "5",
+            name: "node 5",
+            val: 1
+          },
+          {
+            id: "6",
+            name: "node 6",
+            val: 2,
+            selected: true
+          }
+        ],
+        links: [
+          {
+            source: "1",
+            target: "2"
+          },
+          {
+            source: "3",
+            target: "2"
+          },
+          {
+            source: "4",
+            target: "2"
+          },
+          {
+            source: "5",
+            target: "1"
+          }
+        ]
+      }
+    };
+    this.forceGraph = React.createRef();
+    this.forceGraphSettings = {
+      /***************
+       *** STYLES ***
+       ***************/
+      backgroundColor: "#24272B",
+      nodeCanvasObject: (node, ctx, globalScale) => {
+        // create circle
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, 5, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "#636971";
+        ctx.fill();
+        if (node.selected) {
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "#FFF";
+          ctx.stroke();
         }
-      ],
-      links: [
-        {
-          source: "1",
-          target: "2"
-        },
-        {
-          source: "3",
-          target: "2"
-        },
-        {
-          source: "4",
-          target: "2"
-        },
-        {
-          source: "5",
-          target: "1"
+        // create label
+        if (globalScale < 2) return;
+        const label = node.name;
+        const labelOffsetY = -9;
+        ctx.font = `5px Sans-Serif`;
+        ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = "#24272B";
+        ctx.lineWidth = 0.5;
+        ctx.strokeText(label, node.x, node.y - labelOffsetY);
+        ctx.fillStyle = "#636971";
+        ctx.fillText(label, node.x, node.y - labelOffsetY);
+      },
+      linkCanvasObject: (link, ctx, globalScale) => {
+        ctx.beginPath();
+        ctx.moveTo(link.source.x, link.source.y);
+        ctx.lineTo(link.target.x, link.target.y);
+        if (link.source.selected || link.target.selected) {
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "#FFF";
+        } else {
+          ctx.lineWidth = 0.5;
+          ctx.strokeStyle = "#636971";
         }
-      ]
-    }
-  };
+        ctx.stroke();
+      },
+      /***************
+       *** EVENTS ***
+       ***************/
+      onNodeClick: node => {
+        console.log("a node was clicked", node);
+      },
+      onNodeRightClick: node => {
+        console.log("a node was right-clicked", node);
+      },
+      onNodeHover: node => {
+        console.log("a nodeis being hovered", node);
+        this.setState({ hoveredNode: node });
+      },
+      onNodeDrag: node => {
+        console.log("a node is being dragged", node);
+      },
+      onNodeDragEnd: node => {
+        console.log("node no longer being dragged", node);
+      }
+    };
+  }
 
   toggleGraphType = () => {
     const { graphType } = this.state;
@@ -116,9 +183,17 @@ class ForceGraph extends Component {
         </button>
         <button onClick={this.addNode}>add node</button>
         {graphType === 0 ? (
-          <ForceGraph2D graphData={graphData} {...forceGraphSettings} />
+          <ForceGraph2D
+            ref={this.forceGraph}
+            graphData={graphData}
+            {...this.forceGraphSettings}
+          />
         ) : (
-          <ForceGraph3D graphData={graphData} {...forceGraphSettings} />
+          <ForceGraph3D
+            ref={this.forceGraph}
+            graphData={graphData}
+            {...this.forceGraphSettings}
+          />
         )}
       </div>
     );
